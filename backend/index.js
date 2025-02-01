92821e583ae9e4b8429ed7c65d68a33cc7c3d11a
Isaac Jeddiah <isaacjeddiah@gmail.com>
2025-02-02 00:11:53 +0530

    final 1

--- backend/index.js
+++ backend/index.js
@@ -55,12 +55,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { app, server } from "./src/lib/socket.js";
-import { connectDB } from "./src/lin/db.js";
-import authRoutes from "./src/routes/auth.route.js";
-import messageRoutes from "./src/routes/message.route.js";
-import postRoutes from "./src/routes/post.route.js";
-import storyRoutes from "./src/routes/story.route.js";
+import { app, server } from "./lib/socket.js";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import postRoutes from "./routes/post.route.js";
+import storyRoutes from "./routes/story.route.js";
 
 dotenv.config();
 
@@ -69,10 +69,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL,"https://kalakapu.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  exposedHeaders: ['Set-Cookie']
 }));
 
 app.use(cookieParser());
