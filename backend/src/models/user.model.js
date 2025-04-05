2123002354bebf1aee82e3ee65820a00e90b4ea2
Isaac Jeddiah <isaacjeddiah@gmail.com>
2025-04-05 09:25:44 +0530

    updated version for offline or deployment 3

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -11,6 +11,8 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    
+  username: { type: String, required: true, unique: true },
     password: {
       type: String,
       required: true,
